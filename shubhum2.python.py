72fbdb33fbf728a7faeff3459349126f6c7e07ad
Shubhangi Ragit <shubhangiragit81@gmail.com>
2023-06-02 16:59:40 +0530

    first-project

---
+++ shubhum2.python.py
@@ -0,0 +1,6 @@
+##question 3: read the number form user. check if it is positive. if it
+x= float(input('enter the no:'))
+if(x > 0):
+    print('square root is:' , x**(1/2))
+else:
+     print('no is positive')
